ddbdcbe687c026e938547ed8efac4d091ec4d6bd
= <=>
2025-12-20 07:41:40 -0300

    feat/bridge: add go

---
+++ bridge/go/computer.go
@@ -0,0 +1,6 @@
+package main
+
+type Computter interface {
+	Print()
+	SetPrinter(Printer)
+}
---
+++ bridge/go/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	hpPrinter := &Hp{}
+	epsonPrinter := &Epson{}
+
+	macComputer := &Mac{}
+
+	macComputer.SetPrinter(hpPrinter)
+	macComputer.Print()
+	fmt.Println()
+
+	macComputer.SetPrinter(epsonPrinter)
+	macComputer.Print()
+	fmt.Println()
+
+	winComputer := &Windows{}
+
+	winComputer.SetPrinter(hpPrinter)
+	winComputer.Print()
+	fmt.Println()
+}
